4c72fbe212e3817cf1547a442349bff0e1435798
mark <markbeen@virginia.edu>
2020-09-22 21:30:58 -0400

    adding stuff

---
+++ Code/api_keys.py
@@ -0,0 +1,7 @@
+# OpenWeatherMap API Key
+
+api_key = "9d1bc9c17541af5da45aa33626c8a950"
+#weather_api_key = "f81a98c55421ff91c44c3bc4af84569a"
+
+# Google API Key
+g_key = "AIzaSyAvRuKgD7-ehojwGv-f7Ef6XeLWbvTl6xM"
